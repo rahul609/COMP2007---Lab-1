fe5fe6f2deadc5adf00049414f69d8372cdd1f56
rahul609 <rahul609@gmail.com>
2018-02-13 17:36:10 -0500

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Products/Details?Products=[value]
+
+        public ActionResult Details(string apple)
+        {
+
+            // check for an empty products
+            if (apple == null)
+            {
+
+                //return View("Error");
+                return RedirectToAction("Index");
+
+            }
+            ViewBag.apple = apple;
+            return View();
+
+        }
+
+
+    }
+}
